a7e093b586469105a23548a2c508cd9fd472713b
jacobugat <jacobugat@gmail.com>
2026-05-03 20:38:39 -0500

    CRUD

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './interceptors/auth.interceptor'; // Ruta corregida según tu estructura de carpetas
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+// Importamos el nombre exacto que aparece en la terminal: authInterceptor
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // MANTENEMOS TU CONFIGURACIÓN DE ZONE ORIGINAL
     provideZoneChangeDetection({ eventCoalescing: true }), 
     
     provideRouter(routes),
     
-    // Configuramos el cliente HTTP para que soporte interceptores basados en clases
-    provideHttpClient(withInterceptorsFromDi()), 
-    
-    // Registramos el interceptor de seguridad JWT
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    }
+    // Al ser una función interceptora, usamos withInterceptors
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
